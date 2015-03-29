5b513a9d395585b9d5f466e260881812a2834f26
Loïc Knuchel <loicknuchel@gmail.com>
2015-03-29 18:52:30 +0200

    typo

--- www/js/app.js
+++ www/js/app.js
@@ -1,23 +1,23 @@
 angular.module('app', ['ionic', 'firebase'])
 
 .config(function($stateProvider, $urlRouterProvider){
-    $stateProvider
-    .state('app', {
-        url: '/app',
-        templateUrl: 'views/app.html',
-        controller: 'AppCtrl'
-    });
-    $urlRouterProvider.otherwise('/app');
+  $stateProvider
+  .state('app', {
+    url: '/app',
+    templateUrl: 'views/app.html',
+    controller: 'AppCtrl'
+  });
+  $urlRouterProvider.otherwise('/app');
 })
 
-.run(function($ionicPlatform, $rootScope) {
-  $ionicPlatform.ready(function() {
+.run(function($ionicPlatform, $rootScope){
+  $ionicPlatform.ready(function(){
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if(window.cordova && window.cordova.plugins.Keyboard){
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if(window.StatusBar){
       StatusBar.styleDefault();
     }
   });
